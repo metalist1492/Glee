184bca5e95310494a31934ee6afa2ea4ab318f4d
metalist1492 <metalist1492@gmail.com>
2021-11-20 20:20:45 +0200

    added design and features block

--- app/js/main.js
+++ app/js/main.js
@@ -7,12 +7,24 @@ $(function () {
   });
 
   $('[data-fancybox]').fancybox({
-    
+
   });
 
+  var containerEl1 = document.querySelector('[data-ref="container__mixitup-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container__mixitup-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
   $('.products__item').hover(
-    function () { $(this).addClass('products__item--active') },   
-    function () { $(this).removeClass('products__item--active') } 
+    function () { $(this).addClass('products__item--active') },
+    function () { $(this).removeClass('products__item--active') }
   );
   $('.link-box__search, .link-box__cart, .link-box__like').hover(
     function () { $(this).addClass('link-box--active') },
